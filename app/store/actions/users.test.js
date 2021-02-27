a3e872bc719fe7928e358e5e7ec26fcab2795393
Clark Ren <cry3rn@gmail.com>
2021-02-27 07:24:36 -0600

    Added user logout action test

--- app/store/actions/users.test.js
+++ app/store/actions/users.test.js
@@ -1,10 +1,15 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { SET_USER } from 'store/actionTypes';
-import { login } from './users';
+import { login, logout } from './users';
 
-jest.mock('store', () => {});
-jest.mock('extensions/store/hooks', () => {});
+const MOCKED_DEL_PROJECTS = 'delProjects';
+const MOCKED_SEL_PROJECT = 'selProject';
+
+jest.mock('./projects', () => ({
+  delProjects: () => ({ type: MOCKED_DEL_PROJECTS }),
+  selProject: () => ({ type: MOCKED_SEL_PROJECT }),
+}));
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -19,6 +24,19 @@ describe('actions', () => {
     ];
     const store = mockStore();
     store.dispatch(login('userid'));
+    expect(store.getActions()).toMatchObject(expectedActions);
+  });
+  it('removes projects on logout', () => {
+    const expectedActions = [
+      {
+        type: SET_USER,
+        payload: { userId: undefined },
+      },
+      { type: MOCKED_SEL_PROJECT },
+      { type: MOCKED_DEL_PROJECTS },
+    ];
+    const store = mockStore();
+    store.dispatch(logout());
     expect(store.getActions()).toMatchObject(expectedActions);
   });
 });
